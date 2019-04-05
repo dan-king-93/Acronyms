532eb27ebecc2cff5cc14bb45b6243f2a3a47aa2
Daniel <dan.j.king93@gmail.com>
2019-04-05 11:42:37 +0100

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,24 @@
+import Vapor
+import FluentSQLite
+
+// N.B. All Fluent models must be fully codable conformant (for writing to the database)
+final class Acronym: Codable {
+    
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: Content { }
+
+extension Acronym: SQLiteModel { }
+
+// N.B. Can automatically infer scheme for the model due to Codable conformance
+extension Acronym: Migration { }
+
+
